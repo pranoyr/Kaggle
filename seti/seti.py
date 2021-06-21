be88c09d83e3baf51df98764690752a8e56d1e46
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-21 18:34:42 +0530

    metrics updated

--- seti/seti.py
+++ seti/seti.py
@@ -654,7 +654,7 @@ model.to(device)
 
 
 # criterion = nn.BCELoss()
-weights = [0, 1]
+weights = [0, 10]
 weights = torch.FloatTensor(weights).cuda()
 criterion = nn.CrossEntropyLoss(weight=weights)
 optimizer = optim.Adam(model.parameters(), weight_decay=0)
