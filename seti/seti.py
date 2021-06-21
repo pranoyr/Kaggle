eaef34b33c22df42b450b46577cd8cf72d3797a7
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-21 12:53:52 +0530

    metrics updated

--- seti/seti.py
+++ seti/seti.py
@@ -503,6 +503,8 @@ class AverageMeter1:
 		preds = preds.numpy()
 		targets = targets.numpy()
 
+		print(preds.shape)
+
 		# A "micro-average": quantifying score on all classes jointly
 		self.precision["micro"], self.recall["micro"], _ = precision_recall_curve(
 			targets.ravel(), preds.ravel())
