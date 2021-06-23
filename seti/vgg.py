8adda3559b700550d4e2fe4f901302f11084aa0e
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-24 00:29:48 +0530

    added vgg

--- seti/vgg.py
+++ seti/vgg.py
@@ -67,7 +67,7 @@ class VGG(nn.Module):
 
 def make_layers(cfg: List[Union[str, int]], batch_norm: bool = False) -> nn.Sequential:
     layers: List[nn.Module] = []
-    in_channels = 7
+    in_channels = 6
     for v in cfg:
         if v == 'M':
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
