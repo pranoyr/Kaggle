b3334cff8a6b2ff5e20d91b129af9ce5ec7cf3c7
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-24 19:17:33 +0530

    merged

--- seti/seti_val_binary.py
+++ seti/seti_val_binary.py
@@ -710,7 +710,7 @@ def main():
 	# if torch.cuda.device_count() > 1:
 	# 	print("Let's use", torch.cuda.device_count(), "GPUs!")
 	# dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-	model = nn.DataParallel(model)
+	# model = nn.DataParallel(model)
 
 	if resume_path:
 		checkpoint = torch.load(resume_path)
