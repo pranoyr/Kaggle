9d1f17169e89c02952470dc2cc81e054eae6f360
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-27 20:43:53 +0530

    all in one

--- seti/train.py
+++ seti/train.py
@@ -15,6 +15,7 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 from transforms import RandomHorizontalFlip, RandomVerticalFlip
 from albumentations.pytorch.transforms import ToTensorV2
+from transforms import GaussianNoise
 from vgg import vgg16
 import numpy as np
 import random
@@ -696,7 +697,9 @@ def main():
 	train_transform = transforms.Compose([
 		transforms.RandomHorizontalFlip(0.5),
 		transforms.RandomVerticalFlip(p=0.5),
-		transforms.RandomRotation(degrees=(0, 180))
+		transforms.RandomRotation(degrees=(0, 90)),
+		transforms.ColorJitter(brightness=[0.2,1]),
+		GaussianNoise(0.5)
 		# transforms.Normalize(mean=[1.1921e-06,  2.3842e-07,  1.2517e-06,  1.7881e-07,  1.4305e-06,
 		# 						-1.1921e-07], std=[0.0408, 0.0408, 0.0408, 0.0408, 0.0408, 0.0408])
 	])
--- seti/transforms.py
+++ seti/transforms.py
@@ -1,7 +1,8 @@
 import torchvision.transforms.functional as F
 import torchvision.transforms as transforms
+from skimage.util import random_noise
 import torch
-
+import numpy as np
 
 class RandomHorizontalFlip(torch.nn.Module):
     """Horizontally flip the given image randomly with a given probability.
@@ -70,6 +71,38 @@ class RandomVerticalFlip(torch.nn.Module):
                 return img
         return img
 
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(p={})'.format(self.p)
+
+
+
+class GaussianNoise(torch.nn.Module):
+    """Horizontally flip the given image randomly with a given probability.
+    If the image is torch Tensor, it is expected
+    to have [..., H, W] shape, where ... means an arbitrary number of leading
+    dimensions
+
+    Args:
+        p (float): probability of the image being flipped. Default value is 0.5
+    """
+
+    def __init__(self, p=0.5):
+        super().__init__()
+        self.p = p
+
+    def forward(self, img):
+        """
+        Args:
+            img (PIL Image or Tensor): Image to be flipped.
+
+        Returns:
+            PIL Image or Tensor: Randomly flipped image.
+        """
+        if torch.rand(1) < self.p:
+            img = torch.tensor(random_noise(np.array(img), mode='gaussian', mean=0, var=0.001, clip=True))
+            return img
+        return img
 
     def __repr__(self):
         return self.__class__.__name__ + '(p={})'.format(self.p)
