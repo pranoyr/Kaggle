42aeb79db22b15c1be129d077008ec8eff65324c
Pranoy R <pranoyr@Pranoy-2.local>
2021-06-27 20:45:06 +0530

    all in one

--- seti/train.py
+++ seti/train.py
@@ -699,7 +699,7 @@ def main():
 		transforms.RandomVerticalFlip(p=0.5),
 		transforms.RandomRotation(degrees=(0, 90)),
 		transforms.ColorJitter(brightness=[0.2,1]),
-		GaussianNoise(0.5)
+		# GaussianNoise(0.5)
 		# transforms.Normalize(mean=[1.1921e-06,  2.3842e-07,  1.2517e-06,  1.7881e-07,  1.4305e-06,
 		# 						-1.1921e-07], std=[0.0408, 0.0408, 0.0408, 0.0408, 0.0408, 0.0408])
 	])
