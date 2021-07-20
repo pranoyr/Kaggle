41b3324a66cec3ce23d291d643fa7d4a7c3fe6f2
Pranoy R <pranoyr@Pranoy-2.local>
2021-07-21 01:23:38 +0530

    updated

--- seti/train1.py
+++ seti/train1.py
@@ -12,6 +12,8 @@ from eff import EfficientNet
 from vit_pytorch.vit import ViT
 from torch.optim import lr_scheduler
 from torch.nn import BCEWithLogitsLoss
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
 from transforms import RandomHorizontalFlip, RandomVerticalFlip
@@ -92,8 +94,11 @@ class SETIDataset(data.Dataset):
 		# if self.image_set=='train':
 		# 	x = self.transform({"img":torch.from_numpy(x), "target":label})
 		# else:
-		if self.transform:
-			x = self.transform(torch.from_numpy(x).view(3,-1,256).type(torch.FloatTensor))
+		if self.transform:	
+			x = torch.from_numpy(x).view(3,-1,256)
+			x = x.permute(1,2,0).numpy().astype('float32')
+			x = self.transform(image=x)['image']
+			x = x.type(torch.FloatTensor)
 		else:
 			x = torch.from_numpy(x).view(3,-1,256).type(torch.FloatTensor)
 		# x = self.transform(image = x)
@@ -502,6 +507,76 @@ def accuracy(output, target, topk=(1,)):
 
 
 
+class GridMask(DualTransform):
+    
+    def __init__(self, num_grid=3, fill_value=0, rotate=0, mode=0, always_apply=False, p=0.5):
+        super(GridMask, self).__init__(always_apply, p)
+        if isinstance(num_grid, int):
+            num_grid = (num_grid, num_grid)
+        if isinstance(rotate, int):
+            rotate = (-rotate, rotate)
+        self.num_grid = num_grid
+        self.fill_value = fill_value
+        self.rotate = rotate
+        self.mode = mode
+        self.masks = None
+        self.rand_h_max = []
+        self.rand_w_max = []
+
+    def init_masks(self, height, width):
+        if self.masks is None:
+            self.masks = []
+            n_masks = self.num_grid[1] - self.num_grid[0] + 1
+            for n, n_g in enumerate(range(self.num_grid[0], self.num_grid[1] + 1, 1)):
+                grid_h = height / n_g
+                grid_w = width / n_g
+                this_mask = np.ones((int((n_g + 1) * grid_h), int((n_g + 1) * grid_w))).astype(np.uint8)
+                for i in range(n_g + 1):
+                    for j in range(n_g + 1):
+                        this_mask[
+                             int(i * grid_h) : int(i * grid_h + grid_h / 2),
+                             int(j * grid_w) : int(j * grid_w + grid_w / 2)
+                        ] = self.fill_value
+                        if self.mode == 2:
+                            this_mask[
+                                 int(i * grid_h + grid_h / 2) : int(i * grid_h + grid_h),
+                                 int(j * grid_w + grid_w / 2) : int(j * grid_w + grid_w)
+                            ] = self.fill_value
+                
+                if self.mode == 1:
+                    this_mask = 1 - this_mask
+
+                self.masks.append(this_mask)
+                self.rand_h_max.append(grid_h)
+                self.rand_w_max.append(grid_w)
+
+    def apply(self, image, mask, rand_h, rand_w, angle, **params):
+        h, w = image.shape[:2]
+        mask = AF.rotate(mask, angle) if self.rotate[1] > 0 else mask
+        mask = mask[:,:,np.newaxis] if image.ndim == 3 else mask
+        image *= mask[rand_h:rand_h+h, rand_w:rand_w+w].astype(image.dtype)
+        return image
+
+    def get_params_dependent_on_targets(self, params):
+        img = params['image']
+        height, width = img.shape[:2]
+        self.init_masks(height, width)
+
+        mid = np.random.randint(len(self.masks))
+        mask = self.masks[mid]
+        rand_h = np.random.randint(self.rand_h_max[mid])
+        rand_w = np.random.randint(self.rand_w_max[mid])
+        angle = np.random.randint(self.rotate[0], self.rotate[1]) if self.rotate[1] > 0 else 0
+
+        return {'mask': mask, 'rand_h': rand_h, 'rand_w': rand_w, 'angle': angle}
+
+    @property
+    def targets_as_params(self):
+        return ['image']
+
+    def get_transform_init_args_names(self):
+        return ('num_grid', 'fill_value', 'rotate', 'mode')
+
 
 target_names = ['class 0', 'class 1']
 class AverageMeter1:
@@ -703,27 +778,34 @@ def main():
 	device = torch.device("cuda:1" if use_cuda else "cpu")
 
 
-	train_transform = transforms.Compose([
-		transforms.Resize((256,256)),
-		transforms.RandomHorizontalFlip(0.5),
-		transforms.RandomVerticalFlip(p=0.5),
-		transforms.RandomRotation(degrees=(0, 9)),
-		transforms.ColorJitter(brightness=[0.2,1]),
-		# GaussianNoise(0.5)
-		# transforms.Normalize(mean=[1.1921e-06,  2.3842e-07,  1.2517e-06,  1.7881e-07,  1.4305e-06,
-		# 						-1.1921e-07], std=[0.0408, 0.0408, 0.0408, 0.0408, 0.0408, 0.0408])
+
+	train_transform = A.Compose([
+			A.Resize(256,256),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.Rotate(limit=180, p=0.7),
+            A.RandomBrightness(limit=0.6, p=0.5),
+            A.Cutout(
+                num_holes=10, max_h_size=12, max_w_size=12,
+                fill_value=0, always_apply=False, p=0.5
+            ),
+            A.ShiftScaleRotate(
+                shift_limit=0.25, scale_limit=0.1, rotate_limit=0
+            ),
+            A.OneOf([
+                GridMask(num_grid=3, mode=0, rotate=15),
+                GridMask(num_grid=3, mode=2, rotate=15),
+                                ], p=0.7),
+            ToTensorV2(p=1.0),
+
 	])
 
-	# test_transform = transforms.Compose([
-	# 	# transforms.Normalize(mean=[1.1921e-06,  2.3842e-07,  1.2517e-06,  1.7881e-07,  1.4305e-06,
-	# 	# 						-1.1921e-07], std=[0.0408, 0.0408, 0.0408, 0.0408, 0.0408, 0.0408])
-	# ])
 
+	test_transform = A.Compose([
+	A.Resize(256,256),
+	ToTensorV2(p=1.0)
 
-	test_transform = transforms.Compose([
-		transforms.Resize((256,256))])
-
-
+	])
 
 	# train_transform = albumentations.Compose([
 	# albumentations.Resize(224, 224),
