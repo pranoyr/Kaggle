efcce7c88fe9eb0d271f0cc2cb402b5f7c525b2b
Pranoy R <pranoyr@Pranoy-2.local>
2021-07-21 11:11:00 +0530

    updated

--- seti/mean.py
+++ seti/mean.py
@@ -115,13 +115,12 @@ def _resize_image_and_masks(image, self_min_size=800, self_max_size=1333):
 	   
 
 for data in train_loader:
-	images, targets = data
-	# images = images[0]
-	# images = _resize_image_and_masks(images).unsqueeze(0)
-	# images = images.view(images.size(0), images.size(1), -1)
+	images, _ = data
+	batch_samples = images.size(0)
+	images = images.view(batch_samples, images.size(1), -1)
 	mean += images.mean(2).sum(0)
 	std += images.std(2).sum(0)
-	nb_samples += images.size(0)
+	nb_samples += batch_samples
 
 mean /= nb_samples
 std /= nb_samples
