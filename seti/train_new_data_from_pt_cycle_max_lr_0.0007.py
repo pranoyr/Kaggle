e32b0242cc22adac4c769c2029dbca5feff57e52
Pranoy R <pranoyr@Pranoy-2.local>
2021-07-29 17:46:40 +0530

    added last layer

--- seti/train_new_data_from_pt_cycle_max_lr_0.0007.py
+++ seti/train_new_data_from_pt_cycle_max_lr_0.0007.py
@@ -804,7 +804,7 @@ def main():
 	# A.Normalize(mean=[-5.2037e-06, -1.4643e-04,  9.0275e-05], std = [0.9707, 0.9699, 0.9703], max_pixel_value=1, p=1.0),
 #	ToTensorV2(p=1.0)
 
-	])])
+	]),
 
 	# train_transform = A.Compose([
 	# A.Resize(256,256),
@@ -822,7 +822,7 @@ def main():
 	# A.GridDropout( holes_number_x=10, holes_number_y=10, ratio=0.3),
 	# # A.GridDropout(num_grid=3, mode=0, rotate=15)
 	# # A.Normalize(mean=[-5.2037e-06, -1.4643e-04,  9.0275e-05], std = [0.9707, 0.9699, 0.9703], max_pixel_value=1, p=1.0),
-	# ToTensorV2(p=1.0)])
+	ToTensorV2(p=1.0)])
 
 
 	test_transform = A.Compose([
