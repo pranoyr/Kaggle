3aabde953cfbbde8a668f358756b00a07220ae01
Pranoy R <pranoyr@Pranoy-2.local>
2021-07-30 21:47:49 +0530

    grey added

--- seti/predict.py
+++ seti/predict.py
@@ -44,7 +44,7 @@ file = 'submission.csv'
 model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=1, in_channels=3)
 # model = nn.DataParallel(model)
 # load pretrained weights
-checkpoint = torch.load('model.pth')
+checkpoint = torch.load('model.pth', map_location='cpu')
 model.load_state_dict(checkpoint['model_state_dict'])
 model.to(device)
 
